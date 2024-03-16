2d684ec5424a2b054689b27414b01712662ee6dc
Ahmed <ahmedsheb2003@yahoo.com>
2024-03-16 11:32:58 +0200

    initialized electronforge-vite-ts-react project

---
+++ src/app.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.body);
+root.render(<h2 className="text-red-500">Helo from React!</h2>);
