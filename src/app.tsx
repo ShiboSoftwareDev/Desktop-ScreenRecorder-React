ce72d7453f32e304076f10a1d5eb66efdb163195
Ahmed <ahmedsheb2003@yahoo.com>
2024-03-16 11:39:50 +0200

    installed tailwindcss

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,9 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.body);
-root.render(<h2 className="text-red-500">Helo from React!</h2>);
+root.render(
+  <>
+    <h1 className="text-blue-500">Helo from React!</h1>
+    <h2 className="text-green-500">Helo from React!</h2>
+  </>
+);
